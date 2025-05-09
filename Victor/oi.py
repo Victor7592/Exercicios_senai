53f622166552689d9988aeb254b2c1bece9fdbd6
Victor7592 <zyzzfurios@gmail.com>
2025-05-09 15:21:48 -0300

    Add files via upload

---
+++ Victor/oi.py
@@ -0,0 +1,7 @@
+nome = input ('nome do aluno: ')
+nota_1 = float(input ('digite a primeira nota: ' ) ) 
+nota_2 = float(input ('digite a segunda nota: ' ) ) 
+nota_3 = float(input ('digite a terceira nota: ' ) )
+media = (nota_1+nota_2+nota_3) / 3
+print (nome)
+print ('Média',media)   
